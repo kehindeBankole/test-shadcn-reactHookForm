4cee5da8ce6fe2ced0978af6f726385881218d0c
kehindeBankole <bankolek1@gmail.com>
2024-03-02 13:48:10 +0100

    fix typo in css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,3 +30,16 @@ export default function Home() {
     </main>
   );
 }
+
+// Technologies Used:
+
+// React Hook Form: used for form management and validation due to its simplicity, performance, and ease of integration. It provided a convenient way to handle form inputs, perform validation, and manage form state and works beatifully well with typescript.
+
+// Shadcn: Shadcn was utilized for its pre-built components to offer a consistent design system out of the box.
+
+// Next.js: Next.js served as the foundation for the project, providing server-side rendering.
+
+//project was built using next 14 which server renders the app , tailwind which is css framework that allows engineers to write css in our make , create design systems , shadcn which was used for its pre-built components to offer a consistent design system out of the box and react Hook Form used for form management and validation due to its simplicity, performance, and ease of integration. It provided a convenient way to handle form inputs, perform validation, and manage form state and works beatifully well with typescript.
+// the components folder contain shadcn components(ui folder) used in the project , custom components (Header and CompanyForm) and custom icons , this was done to follow shadcn default set up in order to ensure consistency in folder arrangement
+// an assets folder which contains images and font assets can be found in the project
+// on the design i noticed the color of the fonts was "240, 15%, 11%" , so that was set in the globals.css file to make it the default color for the app
